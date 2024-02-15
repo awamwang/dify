8ece4c0099f61a220b1464c6f9a280ec0e86184f
awamwang <wm.keepgoing@gmail.com>
2024-02-15 02:54:49 +0000

    fix:

--- api/core/tools/provider/builtin_tool_provider.py
+++ api/core/tools/provider/builtin_tool_provider.py
@@ -23,7 +23,7 @@ class BuiltinToolProviderController(ToolProviderController):
         provider = self.__class__.__module__.split('.')[-1]
         yaml_path = path.join(path.dirname(path.realpath(__file__)), 'builtin', provider, f'{provider}.yaml')
         try:
-            with open(yaml_path, 'r') as f:
+            with open(yaml_path, 'r', encoding='utf-8') as f:
                 provider_yaml = load(f.read(), FullLoader)
         except:
             raise ToolProviderNotFoundError(f'can not load provider yaml for {provider}')
@@ -53,7 +53,7 @@ class BuiltinToolProviderController(ToolProviderController):
         tool_files = list(filter(lambda x: x.endswith(".yaml") and not x.startswith("__"), listdir(tool_path)))
         tools = []
         for tool_file in tool_files:
-            with open(path.join(tool_path, tool_file), "r") as f:
+            with open(path.join(tool_path, tool_file), "r", encoding='utf-8') as f:
                 # get tool name
                 tool_name = tool_file.split(".")[0]
                 tool = load(f.read(), FullLoader)
